6437c2e0fd9e855083c4c1664dfe647a596b0949
Sam Barker <sjb296@exeter.ac.uk>
2022-02-16 14:09:50 +0000

    Added to Rider class

--- src/cycling/Rider.java
+++ src/cycling/Rider.java
@@ -3,19 +3,13 @@ package src.cycling;
 import java.util.Random;
 import java.util.UUID;
 
-
 public class Rider {
     private String riderName;
     private int riderId;
     private int riderTeamId;
-
     private int riderYearOfBirth;
 
-    Random rand;
-    // Initialise proper random and instantiate
-
-    private int latestRiderId = rand.nextInt();
-    // THEN CHECK THE ID AGAINST ALL OTHERS AND REASSIGN IF NECESSARY
+    private static int latestRiderId = 0;
 
     /**
      * Return riderName.
@@ -27,7 +21,7 @@ public class Rider {
 
     /**
      * Return riderId.
-     * @return
+     * @return riderId The ID of this particular rider.
      */
     public int getRiderId() {
         return this.riderId;
@@ -35,7 +29,7 @@ public class Rider {
 
     /**
      * Return riderTeamId.
-     * @return
+     * @return riderTeamId The ID of the team the rider belongs to.
      */
     public int getRiderTeamId() {
         return this.riderTeamId;
@@ -43,7 +37,7 @@ public class Rider {
 
     /**
      * Return riderYearOfBirth.
-     * @return
+     * @return riderYearOfBirth The rider's year of birth
      */
     public int getRiderYearOfBirth() {
         return riderYearOfBirth;
@@ -51,47 +45,45 @@ public class Rider {
 
     /**
      * Set riderName.
-     * @param newRiderName
+     * @param newRiderName The rider's new name.
      */
     public void setRiderName(String newRiderName) {
         this.riderName = newRiderName;
     }
 
     /**
-     * Set riderTeamId.
-     * @param newRiderTeamId
-     */
-    public void setRiderTeamId(int newRiderTeamId) {
-        this.riderTeamId = newRiderTeamId;
-    }
-
-    /**
      * Set riderYearOfBirth.
-     * @param riderYearOfBirth
+     * @param riderYearOfBirth The rider's new year of birth
      */
     public void setRiderYearOfBirth(int riderYearOfBirth) {
         this.riderYearOfBirth = riderYearOfBirth;
     }
 
     /**
-     * Constructor for Rider.
-     * @param riderName
+     * Constructor
+     * @param riderName Rider's name
+     * @param riderTeamId The ID of the team the rider belongs to
+     * @param riderYearOfBirth The rider's year of birth
      */
-    public Rider(String riderName) {
+    public Rider(String riderName, int riderTeamId, int riderYearOfBirth) {
         this.riderName = riderName;
-        // PLACEHOLDER FOR ID ASSIGNMENT
+        this.riderTeamId = riderTeamId;
+        this.riderYearOfBirth = riderYearOfBirth;
+        this.riderId = latestRiderId++;
     }
 
     /**
      * For back of the napkin testing purposes, comment out when not testing
-     * @param args
+     * @param args Command line args.
      */
-    public static void main(String[] args) {
-        Rider testRider1 = new Rider("John Doe");
+    /*public static void main(String[] args) {
+        Rider testRider1 = new Rider("John Doe", 1997, 1997);
         System.out.println(testRider1.getRiderName());
         System.out.println(testRider1.getRiderId());
-        Rider testRider2 = new Rider("Jane Doe");
+        System.out.println(testRider1.getRiderYearOfBirth());
+        Rider testRider2 = new Rider("Jane Doe", 1998, 2001);
         System.out.println(testRider2.getRiderName());
         System.out.println(testRider2.getRiderId());
-    }
+        System.out.println(testRider2.getRiderYearOfBirth());
+    }*/
 }
