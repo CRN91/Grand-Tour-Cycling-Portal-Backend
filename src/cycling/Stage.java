b999464b96cba01ba13eee514a7c7d97dbe6ccd7
CRN91 <a.kaizra@outlook.com>
2022-02-20 18:11:45 +0000

    Creation of stage class, add constructor to time trial and small variable name update to cycling portal.

--- src/cycling/Stage.java
+++ src/cycling/Stage.java
@@ -1,13 +1,134 @@
 package src.cycling;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 /**
  * Represents a non-time trial stage.
  *
- * @author Sam Barker, Adam Kaizra
+ * @author Adam Kaizra, Sam Barker
  * @version 1.0
  */
 public class Stage {
-  private Segment[] segmentsInStage;
-  private StageType stageType; // CANNOT BE StageType.TT
-  private int stageLengthMetres;
+  protected Integer raceId, id;
+  protected String name, description;
+  protected Double length;
+  protected LocalDateTime startTime;
+  protected StageType stageType;
+  private ArrayList<Segment> segmentsInStage;
+  protected Boolean underDevelopment = true; // State is either under development or waiting results.
+
+  private static int latestId = 0; // enumerates to get unique id, with 2^32 possible ids.
+
+  /**
+   *
+   * @return stage's ID.
+   */
+  public Integer getId() { return id; }
+
+  /**
+   *
+   * @return ID of the staged race this stage is in.
+   */
+  public Integer getRaceId() { return raceId; }
+
+  /**
+   *
+   * @return name of the stage.
+   */
+  public String getName() { return name; }
+
+  /**
+   *
+   * @param name
+   */
+  public void setName(String name) { this.name = name; }
+
+  /**
+   *
+   * @return description of the stage.
+   */
+  public String getDescription() { return description; }
+
+  /**
+   *
+   * @param description
+   */
+  public void setDescription(String description) { this.description = description; }
+
+  /**
+   *
+   * @return length of the stage measured in kilometers.
+   */
+  public Double getLength() { return length; }
+
+  /**
+   *
+   * @param length
+   */
+  public void setLength(Double length) { this.length = length; }
+
+  /**
+   *
+   * @return Local date and time of the start of the race.
+   */
+  public LocalDateTime getStartTime() { return startTime; }
+
+  /**
+   *
+   * @param startTime
+   */
+  public void setStartTime(LocalDateTime startTime) { this.startTime = startTime; }
+
+  /**
+   *
+   * @return terrain type of the stage.
+   */
+  public StageType getStageType() { return stageType; }
+
+  /**
+   *
+   * @param stageType
+   */
+  public void setStageType(StageType stageType) { this.stageType = stageType; }
+
+  /**
+   *
+   * @return array list of all the segments in this stage.
+   */
+  public ArrayList<Segment> getSegmentsInStage() {
+    return segmentsInStage;
+  }
+
+  /**
+   *
+   * @return state of the stage. Either under development (true) or waiting results (false).
+   */
+  public Boolean getUnderDevelopment() { return underDevelopment; }
+
+  /**
+   *
+   * @param state
+   */
+  public void setUnderDevelopment(Boolean state) { this.underDevelopment = state; }
+
+  /**
+   * Constructor
+   *
+   * @param raceId
+   * @param name
+   * @param description
+   * @param length
+   * @param startTime
+   * @param stageType
+   */
+  public Stage(Integer raceId, String name, String description, Double length, LocalDateTime startTime, StageType stageType) {
+    this.raceId = raceId;
+    this.name = name;
+    this.description = description;
+    this.length = length;
+    this.startTime = startTime;
+    this.stageType = stageType;
+    this.id = latestId++;
+  }
 }
